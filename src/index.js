02c1d12dd38135601efeec6797b44ee2cec3105a
Natalia Savchenko <n.savchenko@gmarkg.com>
2023-10-25 00:29:51 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairs = new Map(bracketsConfig);
+
+  let arr = [];
+
+  for (const char of str) {
+    if (char === pairs.get(arr.at(-1))) {
+      arr.pop();
+    } else {
+      arr.push(char);
+    }
+  }
+
+  return arr.length === 0;
 }
